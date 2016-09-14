2207c415617f009fb4daaa603959a47dc194d09e
dtheobald58 <dtheobald58@yahoo.com>
2016-09-14 09:18:44 -0400

    initial commit

---
+++ WinLoseInteger/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinLoseInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userNumb;
+            do
+            {
+                Console.WriteLine("Please enter an integer:");
+                userNumb = int.Parse(Console.ReadLine());
+                if (userNumb % 3 == 0)
+                {
+                    Console.WriteLine("YOU WON!!!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("You lost :(");
+                }
+            } while (userNumb % 3 != 0);
+        }
+    }
+}
